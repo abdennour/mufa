3972a46167f087fa8388e96f54ce9b59c2f615b8
abdennour <mail@abdennoor.com>
2017-01-19 09:58:04 +0300

    إضافة التوثيق

--- src/Mufawwad.js
+++ src/Mufawwad.js
@@ -1,8 +1,43 @@
 import Registry from './Registry';
 
+/**
+ * المُفَوّض  or The **MUFA**wwad is the engine of this library.
+ *  it handles  creation of Mufa instances.
+ *  @class Mufa
+ *  @type {Mufawwad}
+ *  @property {Registry} registry The store
+ *
+ *  @author Abdennour <http://abdennoor.com>
+ *
+ * @example
+ * import Mufa from 'mufa';
+ * const mufa = new Mufa();
+ * // publish
+ * setTimeout(() => {
+ *   mufa.fire('sendEmoji', '👏')
+ * }, 1000)  ;
+ * // subscribe
+ * mufa.on('sendEmoji', (emoji) => console.log(emoji));
+
+ */
 class Mufawwad {
+  /**
+   * The store
+   * @memberof Mufa
+   * @type {Registry}
+   */
   registry = new Registry();
 
+  /**
+   * Subscribe to event and save the callback to be running when the event is published (fired)
+   * @method sub
+   * @memberof Mufa
+   * @param  {String}   event        event name
+   * @param  {Function} callback     callback to be running when the event is published
+   * @param  {Boolean}  [once=false] it restricts subscriber's notification only for the 1ˢᵗ publish
+   * @return {Subscription}                Subscription's info
+   *
+   */
   sub(event, callback, once = false) {
     return this.registry.insert('subscriber', {
       event,
@@ -11,14 +46,14 @@ class Mufawwad {
     });
   }
 
-  on() {
-    return this.sub(...arguments);
-  }
-
-  one() {
-    return this.sub(...arguments, true);
-  }
-
+  /**
+   * Publish (fire) an event & notify event's subscribers immediatly
+   * @method pub
+   * @memberof Mufa
+   * @param  {String}    event  The event name
+   * @param  {*}         data   message to be sent to subscribers
+   * @return {undefined}
+   */
   pub(event, data) {
     const subsToRemove = [];
     this.registry.find(
@@ -33,18 +68,65 @@ class Mufawwad {
 
     if (subsToRemove.length)
       subsToRemove.forEach(id =>
-        this.registry.remove('subscriber', (record) => record.id === id )
-       )
-
-
+        this.registry.remove('subscriber', (record) => record.id === id)
+      )
 
   }
 
+  /**
+   * Subscribe to event and save the callback to be running when the event is published (fired).
+   *
+   * It is an alias of [mufa.sub](#Mufasub)
+   * @method on
+   * @param  {String}   event        event name
+   * @param  {Function} callback     callback to be running when the event is published
+   * @return {Subscription}                Subscription's info
+   *
+   */
+  on() {
+    return this.sub(...arguments);
+  }
+
+  /**
+   * Subscribe once to event and save the callback to be running when the event is published (fired)
+   *
+   * It is almost an alias of [mufa.sub](#Mufasub).
+   * @method one
+   *
+   * @param  {String}   event        event name
+   * @param  {Function} callback     callback to be running when the event is published
+   * @return {Subscription}                Subscription's info
+   *
+   */
+  one() {
+    return this.sub(...arguments, true);
+  }
+
+  /**
+   * Publish (fire) an event & notify event's subscribers immediatly.
+   *
+   * It is an alias of [mufa.pub](#Mufapub)
+   * @method fire
+   * @param  {String}    event  The event name
+   * @param  {*}         data   message to be sent to subscribers
+   * @return {undefined}
+   */
   fire() {
     return this.pub(...arguments);
   }
 
-
 }
 
+/**
+ *
+ * @typedef {Object} Subscription
+ * @property {String} id The ID of subscription
+ * @property {String} event The event name of subscription
+ * @property {Function} callback The action to do when the event is fired
+ *
+ */
+
+/**
+ * @typedef {Array} Registry
+ */
 export default Mufawwad;
