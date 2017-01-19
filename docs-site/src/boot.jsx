4c90cf6744f9b605191fcbd36549e54e7c7f57e3
abdennour <mail@abdennoor.com>
2017-01-19 08:45:17 +0300

    خطأ مطبعي

--- docs-site/src/boot.jsx
+++ docs-site/src/boot.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import Mufa from 'mufa';
-import {subscriptions} from './subs';
+import {subscriptions} from './events';
 const mufa = new Mufa();
 subscriptions(mufa.on.bind(mufa), mufa.fire.bind(mufa), mufa.one.bind(mufa));
 ReactDOM.render(<App mufa={mufa} />, document.getElementById('app'));
